4ec05f50262f3130ca0de7bc68e0f6fd6120026d
dhruv2102 <kumardh@tcd.ie>
2021-09-03 10:26:53 +0000

    Added initial template for testing

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-import os 
-from flask import Flask
+import os
+from flask import (Flask, flash, render_template, 
+                redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 if os.path.exists("env.py"):
     import env
 
 
+# ----------------------- Connectiong to Flask
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def test():
-    return "Hello World"
+@app.route("/get_recepies")
+def get_recepies():
+    recepies = mongo.db.recepies.find()
+    return render_template('recepies.html', recepies=recepies)
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True
-    )
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
