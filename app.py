1965306187da49f0ef5963f7b5e82328f1534eb3
dhruv2102 <kumardh@tcd.ie>
2021-10-09 10:22:51 +0000

    Updated requirements file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from flask import (Flask, flash, render_template,
-                  redirect, request, session, url_for)
+from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
@@ -20,10 +20,10 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/sign_up', methods=["GET", "POST"])
+@app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
@@ -33,23 +33,23 @@ def sign_up():
         # Logic for existing user
         if existing_user:
             flash("Username already exists, try again")
-            return redirect('sign_up')
+            return redirect("sign_up")
 
         # Adding New User
         new_user = {
             "name": request.form.get("name"),
-            "username":  request.form.get('username').lower(),
-            "password": generate_password_hash(request.form.get('password'))
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
         }
         mongo.db.users.insert_one(new_user)
 
         # Adding session cookie
-        session['user'] = request.form.get("username").lower()
-        flash('You are sucessfully signed up!!!')
+        session["user"] = request.form.get("username").lower()
+        flash("You are sucessfully signed up!!!")
 
-        # Redirect to Profile Page------------------------------------------ 
+        # Redirect to Profile Page------------------------------------------
         # Need to do
-    return render_template('sign_up.html')
+    return render_template("sign_up.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -62,23 +62,23 @@ def login():
         if existing_user:
             entered_password = request.form.get("password")
             user_password = existing_user["password"]
-            users_name = existing_user['name']
-            username = existing_user['username']
+            users_name = existing_user["name"]
+            username = existing_user["username"]
 
             if check_password_hash(user_password, entered_password):
-                session["user"] = existing_user['username']
+                session["user"] = existing_user["username"]
                 flash("Welcome {}".format(users_name))
                 # Redirect to profile
-                return redirect(url_for('profile', username=username))           
+                return redirect(url_for("profile", username=username))
             else:
                 flash("Your username/password was wrong")
-                return redirect(url_for('login'))
+                return redirect(url_for("login"))
 
         else:
             flash("User doesn't exist, please sign up")
-            return redirect(url_for('sign_up'))
-    
-    return render_template('login.html')
+            return redirect(url_for("sign_up"))
+
+    return render_template("login.html")
 
 
 @app.route("/get_recepies")
@@ -89,38 +89,25 @@ def get_recepies():
 
 @app.route("/get_individual_recepie/<recepie_id>")
 def get_individual_recepie(recepie_id):
-    recepie = mongo.db.recepies.find_one(
-        {
-            "_id": ObjectId(recepie_id)
-        }
-    )
+    recepie = mongo.db.recepies.find_one({"_id": ObjectId(recepie_id)})
     return render_template("individual_recepie.html", recepie=recepie)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     # Remove user from sesion cookie
-    session.pop('user')
-    return redirect(url_for('home'))
+    session.pop("user")
+    return redirect(url_for("home"))
 
 
-@app.route('/profile/<username>')
+@app.route("/profile/<username>")
 def profile(username):
-    existing_user = mongo.db.users.find_one(
-        {
-            "username": username
-        }
-    )
+    existing_user = mongo.db.users.find_one({"username": username})
 
-    users_name = existing_user['name']
-    recepies = list(
-        mongo.db.recepies.find(
-            {"created_by": username}
-        )
-    )
-    return render_template('profile.html', users_name=users_name, recepie_list=recepies)
+    users_name = existing_user["name"]
+    recepies = list(mongo.db.recepies.find({"created_by": username}))
+    return render_template("profile.html", users_name=users_name, recepie_list=recepies)
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), 
-            debug=True)
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
