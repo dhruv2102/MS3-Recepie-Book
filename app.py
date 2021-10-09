ce6780a21148cafdc959b4ebd0d86d72b313e718
dhruv2102 <kumardh@tcd.ie>
2021-10-09 16:24:09 +0000

    Added route for deleting category

--- app.py
+++ app.py
@@ -192,6 +192,15 @@ def edit_category(category_id):
     return render_template('edit_category.html', category=category)
 
 
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category successfully deleted")
+
+    # Remove the recepies with the given category
+    return redirect(url_for('categories'))
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True
