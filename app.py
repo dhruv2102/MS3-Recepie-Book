1c8399873b14bb6ea4e987203365539166030991
dhruv2102 <kumardh@tcd.ie>
2021-10-18 09:29:24 +0000

    Setting debug as false

--- app.py
+++ app.py
@@ -385,5 +385,5 @@ def is_url_image(image_url):
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True
+        host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=False
     )
