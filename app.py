096578e14087cd44601f1c71a525f10e262dc02d
dhruv2102 <kumardh@tcd.ie>
2021-10-18 09:33:31 +0000

    Making app.y PEP8 compliant

--- app.py
+++ app.py
@@ -385,6 +385,6 @@ def is_url_image(image_url):
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"), port=int(os.environ.get("PORT")), 
+        host=os.environ.get("IP"), port=int(os.environ.get("PORT")),
         debug=False
     )
